66548665c3d5275d80b5d9b9d1abd68adc782130
WinPC <you@example.com>
2020-07-30 22:20:32 +0800

    update

---
+++ Test.py
@@ -0,0 +1,38 @@
+from fp.fp import FreeProxy
+from scholarly import scholarly
+
+
+def set_new_proxy():
+    while True:
+        proxy = FreeProxy(rand=True, timeout=1).get()
+        proxy_works = scholarly.use_proxy(http=proxy, https=proxy)
+        if proxy_works:
+            break
+    print("Working proxy:", proxy)
+    return proxy
+
+
+set_new_proxy()
+
+while True:
+    try:
+        search_query = scholarly.search_pubs('Perception of physical stability and center of mass of 3D objects')
+        print("Got the results of the query")
+        break
+    except Exception as e:
+        print("Trying new proxy")
+        set_new_proxy()
+
+pub = next(search_query)
+print(pub)
+
+while True:
+    try:
+        filled = pub.fill()
+        print("Filled the publication")
+        break
+    except Exception as e:
+        print("Trying new proxy")
+        set_new_proxy()
+
+print(filled)
