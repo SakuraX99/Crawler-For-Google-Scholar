b46aba8dfe81797d1d37552c604f0eaa2967f7f3
WinPC <you@example.com>
2020-07-31 10:02:51 +0800

    update for proxy test

--- Crawler.py
+++ Crawler.py
@@ -1,15 +1,49 @@
+import time
+
 from scholarly import scholarly
+from fp.fp import FreeProxy
+
+def set_new_proxy():
+    while True:
+        proxy = FreeProxy(rand=True, timeout=1).get()
+        proxy_works = scholarly.use_proxy(http=proxy, https=proxy)
+        if proxy_works:
+            break
+    print("Working proxy:", proxy)
+    return proxy
+
+def get_articleInfo(title):
+    while True:
+        try:
+            search_query = scholarly.search_pubs(title)
+            print("Got the results of the query")
+            break
+        except Exception as e:
+            print("Trying new proxy")
+            set_new_proxy()
+
+    pub = next(search_query)
+
+    while True:
+        try:
+            break
+        except Exception as e:
+            print("Trying new proxy")
+            set_new_proxy()
+    return pub
+
 
 source_data = open("iclr_year_conf_title_author_url_biburl_final_2017-2019.txt")
 line = source_data.readline()
 while line:
-    print(line)
+    time_tup = time.localtime(time.time())
+    print("-------------------------------------------------")
+    print(time_tup)
     line = source_data.readline()
     data_array = line.split(" ##### ")
-    print(data_array)
-
-    search_query = scholarly.search_pubs(data_array[2])
-    ret = next(search_query)
+    print(get_articleInfo(data_array[2]))
+    print()
+    print()
 
 
 source_data.close()
