86c3ea43d8920fa7f60c07536ade2a5012eb784d
WinPC <you@example.com>
2020-08-04 22:49:07 +0800

    update

--- Crawler.py
+++ Crawler.py
@@ -1,8 +1,12 @@
 import time
 import pandas as pd
+import logging
+import os
 
 from scholarly import scholarly
 from fp.fp import FreeProxy
+
+logging.basicConfig(filename=os.path.join(os.getcwd(),'log.txt'),level=logging.DEBUG)
 
 data = {"Title":[],
         "cites":[],
@@ -12,10 +16,10 @@ data = {"Title":[],
 def set_new_proxy():
     while True:
         proxy = FreeProxy(timeout=1, rand=True).get()
-        proxy_works = scholarly.use_proxy(http="http://123.179.163.100:53954", https="http://123.179.163.100:53954")
+        proxy_works = scholarly.use_proxy(http=proxy, https=proxy)
         if proxy_works:
             break
-    print("Working proxy:", "http://123.179.163.100:53954")
+    print("Working proxy:", proxy)
     return proxy
 
 def get_articleInfo(title):
@@ -35,22 +39,30 @@ def get_articleInfo(title):
 
 source_data = open("iclr_year_conf_title_author_url_biburl_final_2017-2019.txt")
 line = source_data.readline()
+idx = 0
 while line:
+    idx+=1
     nowTime = time.strftime("%Y-%m-%d %H:%M:%S")
     print("-------------------------------------------------")
     print(nowTime)
     line = source_data.readline()
     data_array = line.split(" ##### ")
-    ret = get_articleInfo(data_array[2])
-    print(ret)
+    try:
+        ret = get_articleInfo(data_array[2])
+        print(ret)
+        json_str = str(ret)
+        json = eval(json_str)
+        print(type(json))
+        print(json["bib"]["cites"])
+        data["Title"].append(json["bib"]["title"])
+        data["cites"].append(json["bib"]["cites"])
+        data["year"].append(json["bib"]["year"])
+    except Exception as e:
+        logging.debug(idx + "fetch error")
+        data["Title"].append("NaN")
+        data["cites"].append("NaN")
+        data["year"].append("NaN")
 
-    json_str = str(ret)
-    json = eval(json_str)
-    print(type(json))
-    print(json["bib"]["cites"])
-    data["Title"].append(json["bib"]["title"])
-    data["cites"].append(json["bib"]["cites"])
-    data["year"].append(json["bib"]["year"])
 
 source_data.close()
 
